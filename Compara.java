5a7e82aca7e47aaa121211591b77ca37a26cc80a
CHUNCHU YAMUNA <125245269+9491623685@users.noreply.github.com>
2023-02-14 21:24:36 +0530

    Add files via upload

---
+++ Compara.java
@@ -0,0 +1,12 @@
+package Java;
+ import java.util.Comparator;
+public class Compara implements Comparator<Integer> {
+
+	public int compare(Integer o1,Integer o2) {
+		if(o1%10>o2%10)
+			return 1;
+		return -1;
+
+	}
+
+}
